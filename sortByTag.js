0f7bcfc5c06eca89e80d196388bdf98c0630cfe6
Alice2506 <ralko84@mail.ru>
2023-10-20 23:26:41 +0300

    added corrections

--- sortByTag.js
+++ sortByTag.js
@@ -1,17 +1,23 @@
 const sortByTag = {
     data() {
         return {
+            // articleArr: [
+            //     {id: 1, tag: "Kitchen", URL: "img/blog_item_1.jpg", title: "Let’s Get Solution for Building Construction Work", date: "26 December,2022", content: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable. Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary."},
+            //     {id: 2, tag: "Bedroom", URL: "img/blog_item_1.jpg", title: "Design sprints are great", date: "26 December,2022", content: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable. Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary."},
+            //     {id: 3, tag: "Building", URL: "img/blog_item_1.jpg", title: "Let’s Get Solution for Building Construction Work", date: "26 December,2022", content: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable. Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary."},
+            // ],
+
             articleArr: [
-                {id: 1, tag: "Kitchen", URL: "img/blog_item_1.jpg", title: "Let’s Get Solution for Building Construction Work", date: "26 December,2022", content: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable. Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary."},
-                {id: 2, tag: "Bedroom", URL: "img/blog_item_1.jpg", title: "Design sprints are great", date: "26 December,2022", content: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable. Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary."},
-                {id: 3, tag: "Building", URL: "img/blog_item_1.jpg", title: "Let’s Get Solution for Building Construction Work", date: "26 December,2022", content: "Lorem ipsum dolor sit amet, adipiscing Aliquam eu sem vitae turpmaximus.posuere in.Contrary to popular belief.There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injecthumour, or randomised words which don't look even slightly believable. Embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary."},
+                {id: 1, URL: "img/blog_item_1.jpg", title: "tomato"},
+                {id: 2, URL: "img/blog_item_1.jpg", title: "lemon"},
+                {id: 3, URL: "img/blog_item_1.jpg", title: "apple"}
             ],
         }
     },
 
     methods: {
         sort() {
-
+            
         }
     },
 
